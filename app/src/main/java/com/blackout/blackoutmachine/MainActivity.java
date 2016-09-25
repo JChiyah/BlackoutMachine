41a6808ba409a8d57ce21834fbabb1c4fdab36ad
Javier Chiyah <fjc3@hw.ac.uk>
2016-09-25 19:36:46 +0100

    Actualización
    Sin countdowntimer, y usando un Thread para pausar los iconos

--- app/src/main/java/com/blackout/blackoutmachine/MainActivity.java
+++ app/src/main/java/com/blackout/blackoutmachine/MainActivity.java
@@ -1,12 +1,13 @@
 package com.blackout.blackoutmachine;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import java.util.Random;
 
 
 public class MainActivity extends Activity {
@@ -14,6 +15,9 @@ public class MainActivity extends Activity {
     protected boolean endGame = false;
     protected OnClickListener bgListener;
     protected FrameLayout rlayout;
+    protected Random rdm = new Random();
+    protected String[] prizes = {"botella", "camiseta", "chupito",
+            "descuento", "gorra", "llavero", "sticker", "powerbank"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,77 +44,78 @@ public class MainActivity extends Activity {
             }
 
         };
-
         rlayout.setOnClickListener(bgListener);
-
     }
 
     protected void startGame() {
         rlayout.setOnClickListener(null);
 
-        new CountDownTimer(3500, 500) {
-            ImageView slot1 = (ImageView) findViewById(R.id.slot1);
-            ImageView slot2 = (ImageView) findViewById(R.id.slot2);
-            ImageView slot3 = (ImageView) findViewById(R.id.slot3);
-            int i = 1;
-
-            public void onTick(long millisUntilFinished) {
-                switch(i) {
-                    case 1:
-                        randomImage(slot1);
-                        i++;
-                    case 2:
-                        randomImage(slot2);
-                        i++;
-                    case 3:
-                        randomImage(slot3);
-                        i = 1;
-                }
-            }
-
-            protected void randomImage(ImageView img) {
-                if(Math.random() < 0.30) {
-                    img.setImageResource(R.drawable.botella);
-                } else if(Math.random() < 0.30) {
-                    img.setImageResource(R.drawable.llavero);
-                } else if(Math.random() < 0.30) {
-                    img.setImageResource(R.drawable.sticker);
-                } else if(Math.random() < 0.30) {
-                    img.setImageResource(R.drawable.gorras);
-                } else if(Math.random() < 0.30) {
-                    img.setImageResource(R.drawable.chupito);
-                } else if(Math.random() < 0.30) {
-                    img.setImageResource(R.drawable.powerbank);
-                } else if(Math.random() < 0.30) {
-                    img.setImageResource(R.drawable.camiseta);
-                }
-            }
-
-            public void onFinish() {
-                slot1.setImageResource(R.drawable.botella);
-                slot2.setImageResource(R.drawable.botella);
-                slot3.setImageResource(R.drawable.botella);
-                ((ImageView)findViewById(R.id.letras)).setImageResource(R.drawable.letraspremio);
-                rlayout.setOnClickListener(bgListener);
-            }
-        }.start();
+        new ImageChanger(8, 250).start();
     }
 
     protected void randomImage(ImageView img) {
-        if(Math.random() < 0.30) {
-            img.setImageResource(R.drawable.botella);
-        } else if(Math.random() < 0.30) {
-            img.setImageResource(R.drawable.llavero);
-        } else if(Math.random() < 0.30) {
-            img.setImageResource(R.drawable.sticker);
-        } else if(Math.random() < 0.30) {
-            img.setImageResource(R.drawable.gorras);
-        } else if(Math.random() < 0.30) {
-            img.setImageResource(R.drawable.chupito);
-        } else if(Math.random() < 0.30) {
-            img.setImageResource(R.drawable.powerbank);
-        } else if(Math.random() < 0.30) {
-            img.setImageResource(R.drawable.camiseta);
+        Resources res = getResources();
+        int resID = res.getIdentifier(prizes[rdm.nextInt(prizes.length)] , "drawable", getPackageName());
+        img.setImageResource(resID);
+    }
+
+    private class ImageChanger extends Thread {
+        final int numberOfTimes;
+        final long sleep;
+
+        public ImageChanger(int numberOfTimes, long sleep) {
+            this.numberOfTimes = numberOfTimes + rdm.nextInt(3);
+            this.sleep = sleep;
+        }
+
+        private void changeImage() {
+            MainActivity.this.runOnUiThread(new Runnable() {
+                public void run() {
+                    randomImage((ImageView)findViewById(R.id.slot1));
+                    randomImage((ImageView)findViewById(R.id.slot2));
+                    randomImage((ImageView)findViewById(R.id.slot3));
+                }
+            });
+        }
+
+        private void displayPrize() {
+            MainActivity.this.runOnUiThread(new Runnable() {
+                public void run() {
+                    Resources res = getResources();
+                    int resID = res.getIdentifier(prizes[rdm.nextInt(prizes.length)] , "drawable", getPackageName());
+
+                    ((ImageView)findViewById(R.id.slot1)).setImageResource(resID);
+                    ((ImageView)findViewById(R.id.slot2)).setImageResource(resID);
+                    ((ImageView)findViewById(R.id.slot3)).setImageResource(resID);
+                }
+            });
+        }
+
+        private void displayWinText() {
+            MainActivity.this.runOnUiThread(new Runnable() {
+                public void run() {
+                    ((ImageView)findViewById(R.id.letras)).setImageResource(R.drawable.letraspremio);
+                    rlayout.setOnClickListener(bgListener);
+                }
+            });
+        }
+
+        @Override
+        public void run() {
+            try {
+                // Juego
+                for(int i = 0; i < numberOfTimes; i++) {
+                    changeImage();
+                    Thread.sleep(sleep);
+                }
+                displayPrize(); // Decidir y poner un premio
+                Thread.sleep(sleep/2);
+                displayWinText(); // Mostrar letras de premio
+
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
     }
 
